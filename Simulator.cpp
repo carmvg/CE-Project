9a2cb8f43ef0ae2a3cece0ba56a94cecbc6ca37b
Caleb Ramey <carmvg@mst.edu>
2022-11-17 22:12:57 -0600

    Added function template

--- Simulator.cpp
+++ Simulator.cpp
@@ -1,12 +1,40 @@
-#include <math.h>
+#include <bitset>
 #include <iostream>
-#include <algorithm>
+#include <assert.h>
+#include <string>
 using namespace std;
 
+const int DIGITS = 32;
+
+//Preforms the restoring division algorithm.
+//Preconditions: Integers Dividened/Divisor 
+string Restoring(int Dividend, int Divisor){
+
+  return 0;
+}
+
+string NonRestoring(int Dividend, int Divisor){
+
+  return 0;
+}
+
 int main(){
-  int Dividend, Divisor;
+  string Dividend;
+  string Divisor;
+  bool finished = false;
 
   //First to be inputted is Dividend, followed by Divisor.
-  
+
+  /*INVARIANT: 
+    {P} while B do C {!B && P}
+    Dividened/Divisor Produce Divised.
+  */
+
+  while (!finished){
+    cin >> Dividend;
+    cin >> Divisor;
+    cout << Dividend << endl << Divisor << endl;
+    
+  }
   return 0;
 }
